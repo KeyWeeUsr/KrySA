de732ef8af12f54be45a193553a4feed1118125b
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-08-13 01:47:32 +0200

    Fix conf #3

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,7 +24,7 @@ import os
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-needs_sphinx = '1.4.4'
+# needs_sphinx = '1.4.4'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
